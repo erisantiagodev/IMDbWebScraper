c8c8347b791b794549a8d5e28192bd1186ce9950
ericaleb97 <43590510+ericaleb97@users.noreply.github.com>
2021-01-05 19:40:31 -0800

    Updated program

--- UpdatedBlazorMovies/UpdatedBlazorMovies/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ UpdatedBlazorMovies/UpdatedBlazorMovies/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -13,77 +13,77 @@ namespace UpdatedBlazorMovies.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\erica\OneDrive\Documents\C# Practice Apps\BlazorProjects\UpdatedBlazorMovies\UpdatedBlazorMovies\_Imports.razor"
+#line 1 "C:\Users\erica\OneDrive\Documents\C#GitHubRepo\IDMbWebScraper\UpdatedBlazorMovies\UpdatedBlazorMovies\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\erica\OneDrive\Documents\C# Practice Apps\BlazorProjects\UpdatedBlazorMovies\UpdatedBlazorMovies\_Imports.razor"
+#line 2 "C:\Users\erica\OneDrive\Documents\C#GitHubRepo\IDMbWebScraper\UpdatedBlazorMovies\UpdatedBlazorMovies\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\erica\OneDrive\Documents\C# Practice Apps\BlazorProjects\UpdatedBlazorMovies\UpdatedBlazorMovies\_Imports.razor"
+#line 3 "C:\Users\erica\OneDrive\Documents\C#GitHubRepo\IDMbWebScraper\UpdatedBlazorMovies\UpdatedBlazorMovies\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\erica\OneDrive\Documents\C# Practice Apps\BlazorProjects\UpdatedBlazorMovies\UpdatedBlazorMovies\_Imports.razor"
+#line 4 "C:\Users\erica\OneDrive\Documents\C#GitHubRepo\IDMbWebScraper\UpdatedBlazorMovies\UpdatedBlazorMovies\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\erica\OneDrive\Documents\C# Practice Apps\BlazorProjects\UpdatedBlazorMovies\UpdatedBlazorMovies\_Imports.razor"
+#line 5 "C:\Users\erica\OneDrive\Documents\C#GitHubRepo\IDMbWebScraper\UpdatedBlazorMovies\UpdatedBlazorMovies\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\erica\OneDrive\Documents\C# Practice Apps\BlazorProjects\UpdatedBlazorMovies\UpdatedBlazorMovies\_Imports.razor"
+#line 6 "C:\Users\erica\OneDrive\Documents\C#GitHubRepo\IDMbWebScraper\UpdatedBlazorMovies\UpdatedBlazorMovies\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\erica\OneDrive\Documents\C# Practice Apps\BlazorProjects\UpdatedBlazorMovies\UpdatedBlazorMovies\_Imports.razor"
+#line 7 "C:\Users\erica\OneDrive\Documents\C#GitHubRepo\IDMbWebScraper\UpdatedBlazorMovies\UpdatedBlazorMovies\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\erica\OneDrive\Documents\C# Practice Apps\BlazorProjects\UpdatedBlazorMovies\UpdatedBlazorMovies\_Imports.razor"
+#line 8 "C:\Users\erica\OneDrive\Documents\C#GitHubRepo\IDMbWebScraper\UpdatedBlazorMovies\UpdatedBlazorMovies\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\erica\OneDrive\Documents\C# Practice Apps\BlazorProjects\UpdatedBlazorMovies\UpdatedBlazorMovies\_Imports.razor"
+#line 9 "C:\Users\erica\OneDrive\Documents\C#GitHubRepo\IDMbWebScraper\UpdatedBlazorMovies\UpdatedBlazorMovies\_Imports.razor"
 using UpdatedBlazorMovies;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\erica\OneDrive\Documents\C# Practice Apps\BlazorProjects\UpdatedBlazorMovies\UpdatedBlazorMovies\_Imports.razor"
+#line 10 "C:\Users\erica\OneDrive\Documents\C#GitHubRepo\IDMbWebScraper\UpdatedBlazorMovies\UpdatedBlazorMovies\_Imports.razor"
 using UpdatedBlazorMovies.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\erica\OneDrive\Documents\C# Practice Apps\BlazorProjects\UpdatedBlazorMovies\UpdatedBlazorMovies\_Imports.razor"
+#line 11 "C:\Users\erica\OneDrive\Documents\C#GitHubRepo\IDMbWebScraper\UpdatedBlazorMovies\UpdatedBlazorMovies\_Imports.razor"
 using MatBlazor;
 
 #line default
@@ -97,7 +97,7 @@ using MatBlazor;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 25 "C:\Users\erica\OneDrive\Documents\C# Practice Apps\BlazorProjects\UpdatedBlazorMovies\UpdatedBlazorMovies\Shared\NavMenu.razor"
+#line 25 "C:\Users\erica\OneDrive\Documents\C#GitHubRepo\IDMbWebScraper\UpdatedBlazorMovies\UpdatedBlazorMovies\Shared\NavMenu.razor"
         private bool collapseNavMenu = true;
 
     private string NavMenuCssClass => collapseNavMenu ? "collapse" : null;
